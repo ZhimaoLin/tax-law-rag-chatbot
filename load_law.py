dcdc17effdde5bba83de1b7a3823b5cda8fe6c23
ZhimaoLin <zhimaolin@outlook.com>
2025-01-19 14:51:36 -0500

    Clean up

--- load_law.py
+++ load_law.py
@@ -1,14 +1,10 @@
 from enum import Enum
-import re
-from typing import Self
-import uuid
-from llmsherpa.readers import LayoutPDFReader
-import json
-from pathlib import Path
 from neo4j import Driver, GraphDatabase
 from pydantic import BaseModel, Field
 import pymupdf
-import pymupdf4llm
+import re
+from typing import Self
+import uuid
 
 from config import Config
 
@@ -27,6 +23,7 @@ class LawHierarchyType(Enum):
     section_l1 = (6, "SectionL1")
     section_l2 = (7, "SectionL2")
     section_l3 = (8, "SectionL3")
+    section_l4 = (9, "SectionL4")
     table_of_contents = (5, "TableOfContents")
     editorial_notes = (5, "EditorialNotes")
     amendments = (6, "Amendments")
@@ -58,12 +55,14 @@ def check_hierarchy_type(title: str) -> LawHierarchyType:
         return LawHierarchyType.editorial_notes
     elif "amendments" in title.lower():
         return LawHierarchyType.amendments
-    elif re.match(r"\([a-z]\)", title):
+    elif re.match(r"\([a-z]\) [A-Z0-9]+", title):
         return LawHierarchyType.section_l1
-    elif re.match(r"\(\d+\)", title):
+    elif re.match(r"\(\d+\) [A-Z0-9]+", title):
         return LawHierarchyType.section_l2
-    elif re.match(r"\([A-Z]\)", title):
+    elif re.match(r"\([A-Z]\) [A-Z0-9]+", title):
         return LawHierarchyType.section_l3
+    elif re.match(r"\([i|v|x]+\) ", title):
+        return LawHierarchyType.section_l4
     else:
         raise ValueError("Unknown Hierarchy Type")
 
@@ -78,8 +77,6 @@ def split_by_header(regex: str, text: str, page_num: int) -> tuple[str, list[Law
             title = match[i]
             content = match[i + 1]
             hierarchy_type = check_hierarchy_type(title)
-            # if hierarchy_type == LawHierarchyType.section:
-            #     print(f"Found Section: {title}")
             new_section = LawSection(hierarchy=hierarchy_type, title=title, text=content, page_num=page_num)
             between.append(new_section)
     else:
@@ -166,12 +163,12 @@ def main():
     stack = [head]
     set_document_node(kg, head)
 
-    for i, page in enumerate(pdf[86:95]):
+    for i, page in enumerate(pdf[86:102]):
         page_num = i + 1
 
         text = page.get_text()
 
-        regex = r"((?:Subtitle [A-Z]|CHAPTER \d+|Subchapter [A-Z]|PART [I|V|X|L|C|D|M]+|§\d+\.|TABLE OF CONTENTS|EDITORIAL NOTES|AMENDMENTS|\([a-z]\) |\(\d+\) |\([A-Z]\) ).*)\n"
+        regex = r"((?:Subtitle [A-Z]|CHAPTER \d+|Subchapter [A-Z]|PART [I|V|X|L|C|D|M]+|§\d+\.|TABLE OF CONTENTS|EDITORIAL NOTES|AMENDMENTS|\([a-z]\) [A-Z0-9]+|\(\d+\) [A-Z0-9]+|\([A-Z]\) [A-Z0-9]+|\([i|v|x]+\) ).*)\n"
 
         before, between = split_by_header(regex=regex, text=text, page_num=page_num)
         stack[-1].text += before
@@ -197,57 +194,3 @@ def main():
 
 main()
 
-
-# pdf_reader = LayoutPDFReader(LLMSHERPA_API_URL)
-# doc = pdf_reader.read_pdf(PDF_PATH)
-
-# with open("output.json", "w") as file:
-#     json.dump(doc.json, file)
-
-
-# markdown = pymupdf4llm.to_markdown(doc=PDF_PATH, page_chunks=True, pages=[86, 87, 88, 89, 90, 91, 92])
-
-# for page in markdown:
-#     with open(f"./test/markdown/{page['metadata']['page']}.md", "w") as file:
-#         file.write(page["text"])
-
-# doc = pymupdf.open(PDF_PATH) # open a document
-
-# for page in doc[86:92]:
-#     text = page.get_text() # extract text from the page
-#     print(text) # print
-
-# raise Exception("Stop here")
-
-# total_pages = len(doc) # get the total number of pages in the document
-# chunk_size = 10 # set the chunk size
-
-# for i in range(0, total_pages, chunk_size):
-#     # Create a new PDF document for the chunk
-#     end_page = min(i + chunk_size - 1, total_pages - 1)
-
-#     new_pdf = pymupdf.open()
-#     new_pdf.insert_pdf(doc, from_page=i, to_page=end_page)
-
-#     chunk_number = (i // chunk_size) + 1
-#     pdf_chunk_name = f"chunk_{chunk_number}_{i+1}_{end_page+1}.pdf"
-#     output_pdf = f"./test/{pdf_chunk_name}.pdf"
-#     new_pdf.save(output_pdf)
-#     new_pdf.close()
-
-
-#     pdf_chunk = pdf_reader.read_pdf(output_pdf)
-#     with open(f"./test/json/{pdf_chunk_name}.json", "w") as file:
-#         try:
-#             json_object = json.dump(pdf_chunk.json, indent=4)
-#             file.write(json_object)
-#             if not json_object:
-#                 print(f"Error in {pdf_chunk_name}: It is empty")
-#         except:
-#             print(f"Error in {pdf_chunk_name}")
-
-
-# for page_num in range(len(doc)):
-#     page = doc[page_num] # get a page
-#     text = page.get_text("text") # extract text from the page
-#     print(text) # print the text
