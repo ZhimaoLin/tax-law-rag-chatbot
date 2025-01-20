bf798d5c5ea8999483f615265df04d790ead74ef
ZhimaoLin <zhimaolin@outlook.com>
2025-01-20 18:41:48 -0500

    Fixed a bug

--- load_csv.py
+++ load_csv.py
@@ -1,12 +1,17 @@
 import csv
 from datetime import datetime
-
+from neo4j import GraphDatabase
 
 from config import Config
-from models.neo4j_db import connect_neo4j_db
 
 
-CSV_FILE = "./data/test.csv"
+CSV_FILE = "./data/tax_data.csv"
+
+
+def connect_neo4j_db() -> None:
+    kg = GraphDatabase.driver(Config.NEO4J_URI, auth=(Config.NEO4J_USERNAME, Config.NEO4J_PASSWORD))
+    kg.verify_connectivity()
+    return kg
 
 
 def main():
